e672b99962af917ade20ba419a7d4abf670095fd
Jason <sjc-925@163.com>
2021-05-14 17:37:09 +0800

    upgrade to spring boot 2.4.5, kotlin 1.4.32

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,23 +2,25 @@ import org.apache.tools.ant.filters.ReplaceTokens
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.4.0"
-    id("io.spring.dependency-management") version "1.0.10.RELEASE"
-    kotlin("jvm") version "1.4.10"
-    kotlin("kapt") version "1.4.10"
-    kotlin("plugin.spring") version "1.4.10"
-    kotlin("plugin.jpa") version "1.4.10"
-    kotlin("plugin.allopen") version "1.4.10"
+    id("org.springframework.boot") version "2.4.5"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    kotlin("jvm") version "1.4.32"
+    kotlin("kapt") version "1.4.32"
+    kotlin("plugin.spring") version "1.4.32"
+    kotlin("plugin.jpa") version "1.4.32"
+    kotlin("plugin.allopen") version "1.4.32"
 }
 
 group = "io.itrunner"
-version = "1.1.0"
+version = "1.1.1"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 val profile: String? by project
 
 repositories {
-    mavenCentral()
+    maven {
+        url = uri("http://192.168.0.253/nexus/repository/maven-public")
+    }
 }
 
 dependencies {
@@ -30,7 +32,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("com.auth0:java-jwt:3.11.0")
+    implementation("com.auth0:java-jwt:3.16.0")
     implementation("io.springfox:springfox-boot-starter:3.0.0")
     kapt("org.springframework.boot:spring-boot-configuration-processor")
     if (profile == "dev") {
@@ -40,7 +42,7 @@ dependencies {
     }
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
-    testImplementation("com.ninja-squad:springmockk:3.0.0")
+    testImplementation("com.ninja-squad:springmockk:3.0.1")
 }
 
 allOpen {
